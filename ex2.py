48c0192b05a776fe493af240b75cd315c46f7ffe
e05kassas <e2238641@cmaisonneuve.qc.ca>
2024-09-11 19:42:35 -0400

    MàJ : Réponse de l'exercice 2.

--- ex2.py
+++ ex2.py
@@ -1,4 +1,27 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
+import math
 
-water_quantity = ...
+# Quantité d'eau de base à assanir.
+qteEauBase = 5
+# Quantité de filtres pour assanir qteEauBase d'eau
+qteFiltresRequise = 1
+# Quantité de lampes pour assanir qteEauBase d'eau
+qteLampesRequise = 3
+# Quantité de chlore (en kilogrammes) pour assanir qteEauBase d'eau
+qteChloreRequise = 0.5
+
+qteEau = float(input("Quelle quantité d'eau faut-il assanir ? "))
+rapportEau = qteEau // qteEauBase
+
+qteFiltres = qteFiltresRequise * rapportEau
+qteLampes = qteLampesRequise * rapportEau
+qteChlore = qteChloreRequise * rapportEau
+
+print("Bonjour"*20)
+
+sortie = (f"Voici les matériaux requis pour l'assainissement de {qteEau} L d'eau :\n" +
+          f"\t- {qteFiltres} filtres\n" + 
+          f"\t- {qteLampes} lampes UV\n" +
+          f"\t- {qteChlore} kg de chlore")
+print(sortie)
